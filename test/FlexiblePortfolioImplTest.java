23aada733cff6f5531000e00584ed33b22c3e110
ganeshvi <ganesh.vi@northeastern.edu>
2022-11-17 14:51:51 -0500

    new tests

--- test/FlexiblePortfolioImplTest.java
+++ test/FlexiblePortfolioImplTest.java
@@ -182,28 +182,94 @@ public class FlexiblePortfolioImplTest {
   }
 
   @Test
-  public void checkDatesDisplay1(){
-
+  public void checkDatesDisplayEveryDay() throws ParseException {
+    Portfolio portfolioObj = new FlexiblePortfolioImpl();
+    JSONObject addTickr = new JSONObject();
+    JSONObject addEntry = portfolioObj.makeTransactionRecord("2022-01-01",
+            7, Integer.valueOf(8), "GOOG");
+    JSONArray listEntry = new JSONArray();
+    listEntry.add(addEntry);
+    addTickr.put("GOOG", listEntry);
+    String path = System.getProperty("user.home") + "/Desktop/PortfolioBucket/"+"pf.json";
+    portfolioObj.savePortfolio(path, addTickr);
+    String expected = "[2021-01-17, 2021-01-18, 2021-01-19, 2021-01-20, 2021-01-21, " +
+            "2021-01-22, 2021-01-23, 2021-01-24, 2021-01-25, 2021-01-26, 2021-01-27," +
+            " 2021-01-28, 2021-01-29, 2021-01-30, 2021-01-31, 2021-02-01, 2021-02-02]";
+    assertEquals(expected,portfolioObj.getDatesDisplay(
+            "2021-01-17","2021-02-02",
+            portfolioObj.checkDifference("2021-01-17","2021-02-02")).toString());
   }
 
   @Test
-  public void checkDatesDisplay2(){
-
+  public void checkDatesDisplayEveryFiveDays() throws ParseException {
+    Portfolio portfolioObj = new FlexiblePortfolioImpl();
+    JSONObject addTickr = new JSONObject();
+    JSONObject addEntry = portfolioObj.makeTransactionRecord("2022-01-01",
+            7, Integer.valueOf(8), "GOOG");
+    JSONArray listEntry = new JSONArray();
+    listEntry.add(addEntry);
+    addTickr.put("GOOG", listEntry);
+    String path = System.getProperty("user.home") + "/Desktop/PortfolioBucket/"+"pf.json";
+    portfolioObj.savePortfolio(path, addTickr);
+    String expected = "[2021-01-17, 2021-01-22, 2021-01-27, 2021-02-01, 2021-02-06, 2021-02-11, " +
+            "2021-02-16, 2021-02-21, 2021-02-26, 2021-03-03, 2021-03-08, 2021-03-13, 2021-03-18," +
+            " 2021-03-23, 2021-03-28, 2021-04-02]";
+    assertEquals(expected,portfolioObj.getDatesDisplay(
+            "2021-01-17","2021-04-02",
+            portfolioObj.checkDifference("2021-01-17","2021-04-02")).toString());
   }
 
   @Test
-  public void checkDatesDisplay3(){
-
+  public void checkDatesDisplayEveryMonth() throws ParseException {
+    Portfolio portfolioObj = new FlexiblePortfolioImpl();
+    JSONObject addTickr = new JSONObject();
+    JSONObject addEntry = portfolioObj.makeTransactionRecord("2022-01-01",
+            7, Integer.valueOf(8), "GOOG");
+    JSONArray listEntry = new JSONArray();
+    listEntry.add(addEntry);
+    addTickr.put("GOOG", listEntry);
+    String path = System.getProperty("user.home") + "/Desktop/PortfolioBucket/"+"pf.json";
+    portfolioObj.savePortfolio(path, addTickr);
+    String expected = "[Jan 2020, Feb 2020, Mar 2020, Apr 2020, May 2020, Jun 2020, Jul 2020, " +
+            "Aug 2020, Sep 2020, Oct 2020, Nov 2020, Dec 2020, Jan 2021]";
+    assertEquals(expected,portfolioObj.getDatesDisplay(
+            "2020-01-17","2021-02-02",
+            portfolioObj.checkDifference("2020-01-17","2021-02-02")).toString());
   }
 
   @Test
-  public void checkDatesDisplay4(){
-
+  public void checkDatesDisplayEveryThreeMonth() throws ParseException {
+    Portfolio portfolioObj = new FlexiblePortfolioImpl();
+    JSONObject addTickr = new JSONObject();
+    JSONObject addEntry = portfolioObj.makeTransactionRecord("2022-01-01",
+            7, Integer.valueOf(8), "GOOG");
+    JSONArray listEntry = new JSONArray();
+    listEntry.add(addEntry);
+    addTickr.put("GOOG", listEntry);
+    String path = System.getProperty("user.home") + "/Desktop/PortfolioBucket/"+"pf.json";
+    portfolioObj.savePortfolio(path, addTickr);
+    String expected = "[Jan 2018, Apr 2018, Jul 2018, Oct 2018, Jan 2019, Apr 2019, Jul 2019, " +
+            "Oct 2019, Jan 2020, Apr 2020, Jul 2020, Oct 2020, Jan 2021]";
+    assertEquals(expected,portfolioObj.getDatesDisplay(
+            "2018-01-17","2021-02-02",
+            portfolioObj.checkDifference("2018-01-17","2021-02-02")).toString());
   }
 
   @Test
-  public void checkDatesDisplay5(){
-
+  public void checkDatesDisplayYearwise() throws ParseException {
+    Portfolio portfolioObj = new FlexiblePortfolioImpl();
+    JSONObject addTickr = new JSONObject();
+    JSONObject addEntry = portfolioObj.makeTransactionRecord("2022-01-01",
+            7, Integer.valueOf(8), "GOOG");
+    JSONArray listEntry = new JSONArray();
+    listEntry.add(addEntry);
+    addTickr.put("GOOG", listEntry);
+    String path = System.getProperty("user.home") + "/Desktop/PortfolioBucket/"+"pf.json";
+    portfolioObj.savePortfolio(path, addTickr);
+    String expected = "[2016, 2017, 2018, 2019, 2020]";
+    assertEquals(expected,portfolioObj.getDatesDisplay(
+            "2016-01-17","2021-02-02",
+            portfolioObj.checkDifference("2016-01-17","2021-02-02")).toString());
   }
 
 
