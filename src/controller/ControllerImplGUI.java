17d6f52c914cbd54f5ede6d03bb88b017b36b1ef
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2022-11-24 15:16:57 -0500

    create portfolio operation

--- src/controller/ControllerImplGUI.java
+++ src/controller/ControllerImplGUI.java
@@ -62,6 +62,102 @@ public class ControllerImplGUI implements Controller, ActionListener {
     }
   }
 
+  private void saveOperation(String pfNameCreate){
+    if(pfNameCreate.length() == 0){
+      guiView.setcreateDialogStatus("Portfolio Name is to be given!!");
+    }
+    else if(pfNameCreate.length() > 0){
+      // validate pf name.
+      if (!checkValidpfName(pfNameCreate)) {
+        guiView.setcreateDialogStatus("Please enter a valid Portfolio name!!");
+      }
+      else if (new File(this.rootDir
+              + pfNameCreate + ".json").exists()) {
+        guiView.setcreateDialogStatus("Portfolio with this name" +
+                pfNameCreate + "already exists!!");
+      }
+    }
+    else{
+      portfolio.savePortfolio(this.rootDir + pfNameCreate + ".json",
+              this.addTickr);
+      guiView.setcreateDialogStatus("Successfully created the portfolio " + pfNameCreate);
+    }
+  }
+  private void addOperation(String pfNameCreate, String tickrCreate,String numStocksCreate,
+                String dateCreate,String commissionCreate){
+    if (pfNameCreate.length() == 0 || tickrCreate.length() == 0 ||
+            numStocksCreate.length() == 0 ||
+            dateCreate.length() == 0) {
+      guiView.setcreateDialogStatus("All the fields are not given!!");
+    }
+    else
+    {
+      Float commission = 0.0f;
+      // validate pf name.
+      if (!checkValidpfName(pfNameCreate)) {
+        guiView.setcreateDialogStatus("Please enter a valid Portfolio name!!");
+      }
+      else if (new File(this.rootDir
+              + pfNameCreate + ".json").exists()) {
+        guiView.setcreateDialogStatus("Portfolio with this name" +
+                pfNameCreate + "already exists!!");
+      }
+      // validate commission fees.
+      else if (commissionCreate.length() > 0){
+        if(portfolio.checkValidInteger(commissionCreate) ||
+                portfolio.checkValidFloat(commissionCreate)){
+          guiView.setcreateDialogStatus("Commission fees given is not a valid number");
+        }
+        else if(Float.valueOf(commissionCreate)<0){
+          guiView.setcreateDialogStatus("Commission fees cannot be negative!");
+        }
+        else{
+          commission = Float.valueOf(commissionCreate);
+        }
+      }
+      // validate date.
+      else if (!portfolio.checkIfRightFormat(dateCreate) || dateCreate.length() < 10) {
+        guiView.setcreateDialogStatus("Date is not entered in YYYY-DD-MM format!");
+      }
+      else if (portfolio.checkFutureDate(dateCreate) ||
+              portfolio.checkTodayDateAndTime(dateCreate)) {
+        guiView.setcreateDialogStatus("You can only enter past date or present(if after " +
+                "9:30am).! Please enter new date");
+      }
+      // validate num of stocks.
+      else if (!portfolio.checkValidInteger(numStocksCreate)) {
+        guiView.setcreateDialogStatus("Number of stocks given is not a valid integer");
+      }
+      // validate tickr symbol.
+      else {
+        try {
+          if(!portfolio.validateTickrSymbol(tickrCreate)){
+            guiView.setcreateDialogStatus("Invalid Tickr Symbol! Enter valid company " +
+                    "tickr symbol!");
+          }
+          else{
+            if (portfolio.checkTickrJSONArray(this.addTickr, tickrCreate)) {
+              guiView.setcreateDialogStatus("The tickr symbol already exists");
+            }
+            else{
+              JSONObject addEntry = portfolio.makeTransactionRecord(dateCreate,
+                      commission, Integer.valueOf(numStocksCreate), tickrCreate);
+              JSONArray listEntry = new JSONArray();
+              listEntry.add(addEntry);
+              this.addTickr.put(tickrCreate, listEntry);
+              guiView.setcreateDialogStatus("Entry added sucessfully!. Click Add to add more " +
+                      "entries or Save to save the portfolio.");
+            }
+          }
+        } catch (FileNotFoundException e) {
+          guiView.setcreateDialogStatus("Exception while validating tickr symbol.");
+          throw new RuntimeException(e);
+        }
+      }
+    }
+
+  }
+
   public boolean checkValidpfName(String pfName) {
     if (pfName == null || pfName.length() > 25 || pfName.isEmpty() || pfName.contains(" ")) {
       return false;
@@ -88,7 +184,6 @@ public class ControllerImplGUI implements Controller, ActionListener {
       break;
       case "Create Portfolio": {
         if (this.rootDirUser.length() == 0) {
-          // set the label text.
           guiView.setCreateLabelStatus("Please specify the root directory path!!");
         } else {
           this.addTickr = new JSONObject();
@@ -97,82 +192,22 @@ public class ControllerImplGUI implements Controller, ActionListener {
       }
       break;
       case "Add": {
-        this.pfNameCreate = guiView.getCreatePfValue();
-        this.tickrCreate = guiView.gettickrcreateValue();
-        this.numStocksCreate = guiView.getnumstockscreateValue();
-        this.dateCreate = guiView.getdateofcreationValue();
-        this.commissionCreate = guiView.getcommissionfeescreateValue();
-        if (this.pfNameCreate.length() == 0 || this.dateCreate.length() == 0 ||
-                this.numStocksCreate.length() == 0 ||
-                this.tickrCreate.length() == 0) {
-          guiView.setcreateDialogStatus("All the values are not given!!");
-        }
-        else {
-          Float commission = 0.0f;
-          if (this.commissionCreate.length() != 0) {
-            // validate commision fees.
-            if(portfolio.checkValidInteger(this.commissionCreate) || portfolio.checkValidFloat(this.commissionCreate)){
-              guiView.setcreateDialogStatus("Commision fees given is not a valid number");
-            }
-            else if(Float.valueOf(this.commissionCreate)<0){
-              guiView.setcreateDialogStatus("Commision fees cannot be negative!");
-            }
-            else{
-              commission = Float.valueOf(this.commissionCreate);
-            }
-          }
-          else {
-            // if pf name exists then print already exists.
-            if (!checkValidpfName(pfNameCreate)) {
-              guiView.setcreateDialogStatus("Please enter a valid Portfolio name!!");
-            } else if (new File(this.rootDir
-                    + pfNameCreate + ".json").exists()) {
-              guiView.setcreateDialogStatus("Portfolio with this name" +
-                      pfNameCreate + "already exists!!");
-            }
-            // validate date.
-            else if (!portfolio.checkIfRightFormat(dateCreate) || dateCreate.length() < 10) {
-              guiView.setcreateDialogStatus("Date is not entered in YYYY-DD-MM format!");
-            } else if (portfolio.checkFutureDate(dateCreate) ||
-                    portfolio.checkTodayDateAndTime(dateCreate)) {
-              guiView.setcreateDialogStatus("You can only enter past date or present(if after " +
-                      "9:30am).! Please enter new date");
-            }
-            // validate num of stocks.
-            else if (!portfolio.checkValidInteger(this.numStocksCreate)) {
-              guiView.setcreateDialogStatus("Number of stocks given is not a valid integer");
-            }
-            // validate tickr symbol.
-            else {
-              try {
-                if(!portfolio.validateTickrSymbol(tickrCreate)){
-                  guiView.setcreateDialogStatus("Invalid Tickr Symbol! Enter valid company " +
-                          "tickr symbol!");
-                }
-                else{
-                  if (portfolio.checkTickrJSONArray(addTickr, this.tickrCreate)) {
-                    guiView.setcreateDialogStatus("The tickr symbol already exists");
-                  }
-                  else if (!portfolio.checkTickrJSONArray(addTickr, this.tickrCreate)){
-                    JSONObject addEntry = portfolio.makeTransactionRecord(this.dateCreate,
-                            commission, Integer.valueOf(this.numStocksCreate), this.tickrCreate);
-                    JSONArray listEntry = new JSONArray();
-                    listEntry.add(addEntry);
-                    addTickr.put(this.tickrCreate, listEntry);
-                  }
-                }
-              } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-              }
-            }
-          }
-        }
+        addOperation(guiView.getCreatePfValue(),guiView.gettickrcreateValue(),
+                guiView.getnumstockscreateValue(), guiView.getdateofcreationValue(),
+                guiView.getcommissionfeescreateValue());
+      }
+      break;
+      case "Save":{
+        saveOperation(guiView.getCreatePfValue());
       }
       break;
       case "Modify Portfolio": {
-        // in modify portfolio.
-        // if path is null then ask the user to first specify the root directory and.
-        // then only click on buttons.
+        if (this.rootDirUser.length() == 0) {
+          guiView.setCreateLabelStatus("Please specify the root directory path!!");
+        }
+        else{
+          guiView.displayModifyPf();
+        }
         // check pf name,date,numStocksLabel,tickr Label,commission label.
         // if commission value is not given take it as zero only.
         // if pf name||date label|| numStocks Label|| tickr label is null then take return a ,
