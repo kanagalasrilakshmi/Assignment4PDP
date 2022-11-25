fab224dbfd2f232056d44db42fa250ab25ec2797
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2022-11-24 22:19:22 -0500

    Update PortfolioNewStratergy.java

--- src/model/PortfolioNewStratergy.java
+++ src/model/PortfolioNewStratergy.java
@@ -7,7 +7,7 @@ public class PortfolioNewStratergy extends FlexiblePortfolioImpl implements Port
                                  Float commissionfees, Float money, String date){
     // take the stocks.
     // divide money by the weight percentages.
-    // subtract that money from commision fees.
+    // subtract that money from commission fees.
     // get the stock value of that stock on that particular date.
     // divide that value by stock price.
     // buy those many stocks on that day for that stock.
@@ -19,7 +19,7 @@ public class PortfolioNewStratergy extends FlexiblePortfolioImpl implements Port
                                       String endDate, Float money){
     // take the stocks.
     // divide the money by weight percentages.
-    // substract that money with commission fees.
+    // subtract that money with commission fees.
     // make transaction for every specified frequency range in the given time range.
     // if end date is not given then, do the transaction till today.
     // if future date is given do transaction till today.
