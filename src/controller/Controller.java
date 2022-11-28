896608b3218198ddf97bc988526eca7cd7398dc6
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2022-11-28 13:05:48 -0500

    reformatted the code

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,11 +7,11 @@ import java.text.ParseException;
  * Interface for the controller.
  */
 public interface Controller {
-  /**
-   * function to run the Stocks implementation.
-   *
-   * @throws ParseException when parsing of a date fails.
-   * @throws IOException    when given input is not valid
-   */
-  void goStocks() throws ParseException, IOException;
+    /**
+     * function to run the Stocks implementation.
+     *
+     * @throws ParseException when parsing of a date fails.
+     * @throws IOException    when given input is not valid
+     */
+    void goStocks() throws ParseException, IOException;
 }
