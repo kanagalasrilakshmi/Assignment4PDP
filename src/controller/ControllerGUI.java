f58b3528b84fbfe6a809d5f7dd39cc5efcbca717
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2022-11-28 22:52:02 -0500

    base comments for adding comments

--- src/controller/ControllerGUI.java
+++ src/controller/ControllerGUI.java
@@ -4,33 +4,108 @@ import java.io.FileNotFoundException;
 import java.text.ParseException;
 
 public interface ControllerGUI extends Controller {
+  /**
+   * Set the root directory based on the input given by the user.
+   */
   void setDirectory();
 
+  /**
+   *
+   * @param label can be
+   */
   void displayDialogPane(String label);
 
+  /**
+   *
+   * @param pfNameCreate
+   * @param tickrCreate
+   * @param numStocksCreate
+   * @param dateCreate
+   * @param commissionCreate
+   * @throws FileNotFoundException
+   */
   void addOperation(String pfNameCreate, String tickrCreate, String numStocksCreate,
                     String dateCreate, String commissionCreate) throws FileNotFoundException;
 
+  /**
+   *
+   * @param pfNameCreate
+   */
   void saveOperation(String pfNameCreate);
 
+  /**
+   *
+   * @param pfNameModify
+   * @param tickrModify
+   * @param numStocksModify
+   * @param dateModify
+   * @param commissionModify
+   * @param statuslabel
+   * @throws FileNotFoundException
+   * @throws ParseException
+   */
   void modifyValidate(String pfNameModify, String tickrModify, String numStocksModify,
                       String dateModify, String commissionModify, String statuslabel)
           throws FileNotFoundException, ParseException;
 
+  /**
+   *
+   * @param pfNamedate
+   * @param dateValue
+   * @throws FileNotFoundException
+   * @throws ParseException
+   */
   void validateDateVal(String pfNamedate, String dateValue)
           throws FileNotFoundException, ParseException;
 
+  /**
+   *
+   * @param pfNameBasis
+   * @param dateBasis
+   * @throws ParseException
+   */
   void validateCostBasis(String pfNameBasis, String dateBasis) throws ParseException;
 
+  /**
+   *
+   * @param pfNameComposition
+   */
   void getCompositionpf(String pfNameComposition);
 
+  /**
+   *
+   * @param stratergydollarexistname
+   * @param dollarexistpfname
+   * @param stocksexist
+   * @param weightsexist
+   * @param dollarexistval
+   * @param dollarexistdate
+   * @param dollarexistcommision
+   */
   void validateExistingDollar(String stratergydollarexistname, String dollarexistpfname,
                               String stocksexist, String weightsexist, String dollarexistval,
                               String dollarexistdate, String dollarexistcommision);
 
+  /**
+   *
+   * @param stratergydollarnewname
+   * @param dollarnewcreatepfname
+   * @param stocksnew
+   * @param weightsnew
+   * @param dollarnewval
+   * @param dollarnewdays
+   * @param dollarnewstartdate
+   * @param dollarnewenddate
+   * @param dollarnewcommission
+   * @throws ParseException
+   */
   void validateNewDollar(String stratergydollarnewname, String dollarnewcreatepfname,
                          String stocksnew, String weightsnew, String dollarnewval,
                          String dollarnewdays, String dollarnewstartdate, String dollarnewenddate,
                          String dollarnewcommission) throws ParseException;
+
+  /**
+   *
+   */
   void displaysetrootpane();
 }
--- src/view/GUIView.java
+++ src/view/GUIView.java
@@ -3,129 +3,368 @@ package view;
 import controller.ControllerGUI;
 
 public interface GUIView {
-
+    /**
+     *
+     */
     void makeVisible();
 
+    /**
+     *
+     * @return
+     */
     String givenPath();
 
+    /**
+     *
+     * @param setMessage
+     */
     void setpathStore(String setMessage);
 
+    /**
+     *
+     */
     void displayCostBasis();
 
+    /**
+     *
+     * @param message
+     */
     void setLabelCostBasisStatus(String message);
 
+    /**
+     *
+     */
     void displayCreatePf();
 
+    /**
+     *
+     */
     void displayModifyPf();
 
+    /**
+     *
+     */
     void displayValuepf();
 
+    /**
+     *
+     * @param message
+     */
     void setCreateLabelStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setcreateDialogStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setModifyLabelStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setmodifyDialogStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setValueLabelStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setvalueDialogStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setCostBasisDialogStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setCreatePfValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void settickrmodifyValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void settickrcreateValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setnumstockscreateValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdateofcreationValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setcommissionfeescreateValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setModifyPfValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setnumstocksmodifyValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdateofmodifynValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setcommissionfeesmodifyValue(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setpfnameVal(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdateVal(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setpfNameCostBasis(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setDate(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setportfoliosListModify(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setPortfoliosListVal(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setPortfoliosListBasis(String message);
 
+    /**
+     *
+     * @param features
+     */
     void addFeatures(ControllerGUI features);
 
+    /**
+     *
+     * @param message
+     */
     void setRetrievePanelStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setPortfoliosListRetrieve(String message);
 
+    /**
+     *
+     */
     void displayRetrievepf();
 
+    /**
+     *
+     * @param message
+     */
     void setretrieveDialogStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setpfnameretrieve(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setPortfoliosListComposition(String message);
 
+    /**
+     *
+     */
     void displayDollarExistingpf();
 
+    /**
+     *
+     */
     void displayDollarNewpf();
 
+    /**
+     *
+     * @param message
+     */
     void setdollarExistingStatus(String message);
 
+    /**
+     *
+      * @param message
+     */
     void setdollarNewStatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdollarexistpanestatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setpfNameExistDollar(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdollardateexist(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdollarexistcommisionval(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setstocksexist(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setweightsexist(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdollarexistval(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdollarnewpanestatus(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setportfolioslistdollarexist(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setpfnamedollarnew(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setstocksnew(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setweightsnew(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdollarnewval(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setstartdatenew(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setenddatenew(String message);
 
+    /**
+     *
+     * @param message
+     */
     void setdollardays(String message);
 
+    /**
+     * Sets the value for the commission value for the commission field in start to finish dialog pane
+     * @param message is the value
+     */
     void setdollarcommissionnew(String message);
 
+    /**
+     * Sets the value for the strategy name while adding dollar strategy on existing portfolio.
+     * @param message is the value that needs to be set for the strategy field while adding strategy rtfolio pane
+     */
     void setstrategynameexist(String message);
 
+    /**
+     * Sets the value for the strategy name while creating start to finish portfolio.
+     * @param message that needs to be set for the strategy field in start to finish dialog pane
+     */
     void setstrategynamenewexist(String message);
 
 }
